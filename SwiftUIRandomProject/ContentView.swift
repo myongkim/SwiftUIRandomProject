3c2be96abb95c0ad672de38b11065cf7bdb0b749
Isaac Kim <myong328@gmail.com>
2022-04-05 09:40:36 -0700

    toggle Button SwiftUI

--- SwiftUIRandomProject/ContentView.swift
+++ SwiftUIRandomProject/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var agreedToTerms = false
+    @State private var agreedToPrivacyPolicy = false
+    @State private var agreedToEmails = false
+    
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        let agreedToAll = Binding<Bool> (
+            get: { agreedToTerms && agreedToPrivacyPolicy && agreedToEmails},
+            set: {
+                agreedToTerms = $0
+                agreedToPrivacyPolicy = $0
+                agreedToEmails = $0
+            }
+        )
+        
+        return VStack {
+            Toggle("Agree to terms", isOn: $agreedToTerms)
+            Toggle("Agree to privacy policy", isOn: $agreedToPrivacyPolicy)
+            Toggle("Agree to receive shipping emails", isOn: $agreedToEmails)
+            Toggle("Agree to all", isOn: agreedToAll)
+        }
     }
 }
 
