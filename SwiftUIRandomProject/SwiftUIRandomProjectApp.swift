b99f658379b111aca4cdd3aecd0130f0e7b573f0
Isaac Kim <myong328@gmail.com>
2022-04-05 09:22:46 -0700

    Initial Commit

---
+++ SwiftUIRandomProject/SwiftUIRandomProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRandomProjectApp.swift
+//  SwiftUIRandomProject
+//
+//  Created by Isaac Kim on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRandomProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
